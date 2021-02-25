075d319c6830493ebc73deaf719c02c40ec27d81
Stephen A. Wood <saw@jlab.org>
2021-02-25 09:29:00 -0500

    Start SBS configuration

---
+++ apps/iocBoot/ioccaenhv/st.cmd
@@ -0,0 +1,40 @@
+#!../../bin/linux-x86_64/ioccaen
+#
+
+< envPaths
+epicsEnvSet("IOC","ioccaenhv")
+
+cd ${TOP}
+
+## Register all support components
+dbLoadDatabase("dbd/ioccaen.dbd")
+ioccaen_registerRecordDeviceDriver(pdbbase)
+
+Init_CAEN()
+
+Start_CAEN(0, "129.57.37.95")
+
+# Load record instances
+
+#dbLoadRecords("$(DEVIOCSTATS)/db/iocAdminSoft.db", "IOC=$(IOC)")
+#dbLoadRecords("db/save_restoreStatus.db","P=${IOC}:")
+
+dbLoadRecords("db/caenchassis.db","CrName=BBTH-HV,CScode=#C0")
+#dbLoadRecords("db/caenchassis.db","CrName=HVBAND2,CScode=#C2")
+
+dbLoadTemplate("db/HVBBHODO.substitutions")
+
+cd ${TOP}/iocBoot/${IOC}
+
+#< save_restore.cmd
+
+#asSetFilename("../acf/caenhv.acf")
+iocInit()
+
+#caPutLogInit("clonioc1:7011")
+
+#makeAutosaveFiles()
+#create_monitor_set("info_positions.req","","")
+#create_monitor_set("info_settings.req","","")
+
+dbl > BBTH-HV-pv.list
